8ed512e4da3eab07d4f129060e85b30c9fbdd6a3
cole-dav <56798972+cole-dav@users.noreply.github.com>
2023-04-01 20:22:59 -0400

    pack and open pack place holders

--- src/components/Jackpot.js
+++ src/components/Jackpot.js
@@ -1,6 +1,8 @@
 import React from 'react'
-import { Modal, Frame, ProgressBar } from '@react95/core'
+import { Modal, Frame,Tabs, Input, Button } from '@react95/core'
+// import { } from '@react95/core/components';
 // import '@react95/icons/icons.css';
+// import { , Tab } from '../components/Tabs';
 
 
 
@@ -33,8 +35,23 @@ function Jackpot({ items, closeJackpot, openNotepad, isMobile }) {
                 height="100%"
             >
                 
-               <ProgressBar style={{ flex: 1 }} width={isMobile ? 150 : 200} percent={30} />
-               
+                {/* <ProgressBar style={{ flex: 1 }} width={isMobile ? 150 : 200} percent={30} /> */}
+                <br />
+                <Tabs style={{ width: 350 }} defaultActiveTab="Portfolio">
+                    <Tabs.Tab title="Portfolio">
+                        <div>
+                            <h2>Account</h2>
+                            <p>Rank 1: Josh Allen</p>
+                        </div> 
+                    </Tabs.Tab>
+                    <Tabs.Tab title="Pack">
+                        <Button style={{ width: 230, marginLeft: 22 }} > Open Pack</Button>    
+                    </Tabs.Tab>
+                    <Tabs.Tab title="Collection">
+                        <Input style={{ width: 180, marginLeft: 22 }} />    
+                    </Tabs.Tab>
+                </Tabs>
+                
 
             </Frame>
         </Modal>
