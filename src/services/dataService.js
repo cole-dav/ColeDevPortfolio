60ce92dc6f0a9504396360e4e2d3146ace94488d
cole-dav <56798972+cole-dav@users.noreply.github.com>
2023-09-30 17:52:18 -0400

    update

--- src/services/dataService.js
+++ src/services/dataService.js
@@ -78,7 +78,7 @@ export default class DataService {
               title: 'Jackpot ',
               description:
                 'Jackpot is a sports consumption revolution ',
-              techStack: 'React, Firebase, Flow',
+              techStack: 'React Native, Supabase',
               myRole: 'Founder',
               // url: 'https://pitch.com/public/ccc225dd-fe63-43b5-aa44-ddd1121add0b',
               // repo: 'https://github.com/cole-dav',
@@ -121,73 +121,69 @@ export default class DataService {
               name: 'Databricks',
               progress: 70,
             },
-            {
-              name: 'Blockchain',
-              progress: 60,
-            },
           ],
           soft: 'Analytical Thinking, Entrepreneurship, Creative Problem Solving, Leadership, Teamwork ',
         },
       },
-      {
-        id: 'PeterResume',
-        name: 'PeterResume.txt',
-        icon: 'notepad_2',
-        content: {
-          resumeLink:
-            'https://docs.google.com/document/d/1Wp9BQQXdWX_MHDb_Beq70eJDQGPXU9PC1Q50EK--VrY/export?format=pdf',
-          workExperience: [
-            {
-              jobTitle: 'Investment Banking Summer Analyst',
-              company: 'Citigroup (NYSE: C)',
-              location: 'New York',
-              period: 'JUNE 2023 – AUG 2023',
-              accomplishments: [
-                'Incoming Investment Banking summer analyst for GSP Financing and Securitization',
+      // {
+      //   id: 'PeterResume',
+      //   name: 'PeterResume.txt',
+      //   icon: 'notepad_2',
+      //   content: {
+      //     resumeLink:
+      //       'https://docs.google.com/document/d/1Wp9BQQXdWX_MHDb_Beq70eJDQGPXU9PC1Q50EK--VrY/export?format=pdf',
+      //     workExperience: [
+      //       {
+      //         jobTitle: 'Investment Banking Summer Analyst',
+      //         company: 'Citigroup (NYSE: C)',
+      //         location: 'New York',
+      //         period: 'JUNE 2023 – AUG 2023',
+      //         accomplishments: [
+      //           'Incoming Investment Banking summer analyst for GSP Financing and Securitization',
                 
-              ],
-            },
-            {
-              jobTitle: 'Statim Operator',
-              company: 'Tempus Ex Machina',
-              location: 'Atlanta',
-              period: 'AUG 2021 - PRESENT',
-              accomplishments: [
-                'Facilitated season long play-by-play data collection for the NFL and PAC-12 via Statim application',
-                'Contributed to implementation of upgrades and modifications to the application during the inaugural season',
-                'Responsible for setup of office data feed on game days and assisting operators through technical difficulties ',
-                'Reported bugs and suggested new features to the dev. team for quality assurance and user-experience upgrades',
-              ],
-            },
-            {
-              jobTitle: 'Corporate Finance Summer Analyst',
-              company: 'VICI Properties',
-              location: 'New York',
-              period: 'JUNE 2022 – AUG 2022',
-              accomplishments: [
-                'Analyzed and summarized the $17.2bn acquisition of MGM Growth Properties by VICI Properties',
-                'Created a model to estimate & compare WACC of competing REITs accounting for new Fed rates',
-                'Authored a white paper on an emerging experiential industry and presented opportunities to managment',
-              ],
-            },
-          ],
-          education: [
-            {
-              credit: 'BS and MS in Economics',
-              place: 'Georgia Institute of Technology',
-              gpa: '3.5/4.0 GPA',
-              period: 'AUG 2019 - JULY 2024',
-            },
-          ],
-        },
-      },
+      //         ],
+      //       },
+      //       {
+      //         jobTitle: 'Statim Operator',
+      //         company: 'Tempus Ex Machina',
+      //         location: 'Atlanta',
+      //         period: 'AUG 2021 - PRESENT',
+      //         accomplishments: [
+      //           'Facilitated season long play-by-play data collection for the NFL and PAC-12 via Statim application',
+      //           'Contributed to implementation of upgrades and modifications to the application during the inaugural season',
+      //           'Responsible for setup of office data feed on game days and assisting operators through technical difficulties ',
+      //           'Reported bugs and suggested new features to the dev. team for quality assurance and user-experience upgrades',
+      //         ],
+      //       },
+      //       {
+      //         jobTitle: 'Corporate Finance Summer Analyst',
+      //         company: 'VICI Properties',
+      //         location: 'New York',
+      //         period: 'JUNE 2022 – AUG 2022',
+      //         accomplishments: [
+      //           'Analyzed and summarized the $17.2bn acquisition of MGM Growth Properties by VICI Properties',
+      //           'Created a model to estimate & compare WACC of competing REITs accounting for new Fed rates',
+      //           'Authored a white paper on an emerging experiential industry and presented opportunities to managment',
+      //         ],
+      //       },
+      //     ],
+      //     education: [
+      //       {
+      //         credit: 'BS and MS in Economics',
+      //         place: 'Georgia Institute of Technology',
+      //         gpa: '3.5/4.0 GPA',
+      //         period: 'AUG 2019 - JULY 2024',
+      //       },
+      //     ],
+      //   },
+      // },
       {
         id: 'contact',
         name: 'Contact.txt',
         icon: 'inetcfg_2301',
         content: {
           emailText:
-            'If you want to hire me or invite to a project, just email me on ',
+            'If you want to hire me or collaberate on a project, just email me at ',
           email: 'dc@gatech.edu',
           socialText: 'Or you can reach me out through social media:',
           social: [
--- src/utils/startWebamp.js
+++ src/utils/startWebamp.js
@@ -3,20 +3,21 @@ import Webamp from 'webamp'
 const startWebamp = () => {
     if (Webamp.browserIsSupported()) {
         const webamp = new Webamp({
-            initialTracks: [{
-                metaData: {
-                    artist: "Sony",
-                    title: "Jungle Mix"
-                },
-                url: `${process.env.PUBLIC_URL}/jungle.mp3`,
-                duration: 3729,
-            },
+            initialTracks: [
             {
                 metaData: {
                     artist: "BabyTron",
                     title: "Melrose"
                 },
                 url: `${process.env.PUBLIC_URL}/Babytron-Melrose.mp3`,
+                duration: 81,
+            },
+            {
+                metaData: {
+                    artist: "Untitled",
+                    title: "mystery"
+                },
+                url: `${process.env.PUBLIC_URL}/DIAND.mp3`,
                 duration: 81,
             },
             {
@@ -29,20 +30,21 @@ const startWebamp = () => {
             },
             {
                 metaData: {
-                    artist: "Future",
-                    title: "Comin Out Strong"
-                },
-                url: `${process.env.PUBLIC_URL}/Future-Comin-Out-Strong.mp3`,
-                duration: 250,
-            },
-            {
-                metaData: {
                     artist: "Lil Yachty",
                     title: "drive ME crazy!"
                 },
                 url: `${process.env.PUBLIC_URL}/Lil-Yachty-drive-ME-crazy!.mp3`,
                 duration: 247,
-            }]
+            },
+            {
+                metaData: {
+                    artist: "Sony",
+                    title: "Jungle Mix"
+                },
+                url: `${process.env.PUBLIC_URL}/jungle.mp3`,
+                duration: 3729,
+            },
+        ]
         });
         webamp.onClose(() => {
             webamp.dispose();
